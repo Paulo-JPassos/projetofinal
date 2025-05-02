73742f329c0544afc6fc8b38f5d5a9ccc776737c
Paulo-JPassos <paulojpassos@gmail.com>
2025-05-01 21:21:27 -0300

    Add files via upload

--- projetofinal.py
+++ projetofinal.py
@@ -34,4 +34,14 @@ colunas_utilizadas = [
 
 df = df[[col for col in colunas_utilizadas if col in df.columns]]
 
+# Filtro por estado
+ufs = df["CO_UF"].dropna().unique()
+estado = st.selectbox("Selecione um estado para análise", sorted(ufs))
 
+df_estado = df[df["CO_UF"] == estado]
+
+# Gráfico 1: Tipos de unidade no estado
+st.subheader(f"Distribuição por Tipo de Unidade - {estado}")
+tipo_unidade = df_estado["DS_TIPO_UNIDADE"].value_counts().reset_index()
+fig1 = px.bar(tipo_unidade, x="index", y="DS_TIPO_UNIDADE", labels={"index": "Tipo", "DS_TIPO_UNIDADE": "Quantidade"}, color="index")
+st.plotly_chart(fig1, use_container_width=True)
